b7fa86cb061cd043aaa2fc29ad51d9ee969e7b1b
imalka <imalkachandrasena@gmail.com>
2024-09-12 14:44:29 +0530

    validated the ID,BookName and Barcode fields

--- app/src/main/java/school/actions/BookRegUISave.java
+++ app/src/main/java/school/actions/BookRegUISave.java
@@ -8,9 +8,28 @@ import school.model.Book;
 public class BookRegUISave extends SaveAction {
     @Override
     public void execute() throws Exception {
+        if(validateFields()){
+            Book book = (Book) getViewModel("Book");
+            F.persist(book);
+            addMessage("msg","Book saved successfully");
+            updateViewModel("Book", null);
+        }
+    }
+
+    private boolean validateFields(){
         Book book = (Book) getViewModel("Book");
-        F.persist(book);
-        addMessage("msg","Book saved successfully");
-        updateViewModel("Book", null);
+        if(book.getId() == null){
+            addMessage("msg","Please enter the ID number");
+            return false;
+        }
+        if(book.getBookName() == null){
+            addMessage("msg","Please enter the Book Name");
+            return false;
+        }
+        if(book.getBarcode() == null){
+            addMessage("msg","Please enter the barcode number");
+            return false;
+        }
+        return true;
     }
 }
