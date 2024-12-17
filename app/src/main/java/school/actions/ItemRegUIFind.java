eab73807d74c4b339a9b98bb2978f06bbccaf83c
fasminarismiya <fasminarismiya17@gmail.com>
2024-12-17 20:25:07 +0530

    modify find

--- app/src/main/java/school/actions/ItemRegUIFind.java
+++ app/src/main/java/school/actions/ItemRegUIFind.java
@@ -5,11 +5,31 @@ import com.androix.FBaseAction;
 import school.model.Item;
 import school.model.Student;
 
+
+
 public class ItemRegUIFind extends FBaseAction {
     @Override
     public void execute() throws Exception {
-        Item s= (Item) getViewModel("Item");
-        addMessage("msg",
-                s.getItemName()+" "+s.getItemQuantity()+" "+s.getItemPrice()+" "+s.getItemDistributor());
+        if(validate()){
+            Item item = (Item) getViewModel("Item");
+            Long iId = item.getId();
+            Item item1 = F.find(Item.class,iId);
+            updateViewModel("Item",item1);
+        }
+    }
+    private boolean validate(){
+        Item item = (Item) getViewModel("Item");
+        if(item.getId() != null){
+            Long iId = item.getId();
+            Item item2 = F.find(Item.class,iId);
+            if(item2 == null){
+                addMessage("msg","No such Item ID.");
+                return false;
+            }
+        }else{
+            addMessage("msg","Enter the ID.");
+            return false;
+        }
+        return true;
     }
 }
